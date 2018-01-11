260c6b8edea888f0e145f3e94bf9549ca0d7b481
takumi.I <nkc17.takumi.ito@gmail.com>
2018-01-11 14:06:37 +0900

    UI周りの配置したよー あと生命力は増えるようになってるよー

---
+++ Assets/game/game_script/gameMain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameMain : MonoBehaviour
+{
+    [HideInInspector]
+    public float gameDeltaTime;//ゲームスピードを考慮した1fの時間
+    [SerializeField]
+    private float playSpeed; //ゲームの速度、アイテムとかスキルで増減させるかな
+    public float costMax, costValue, costUpVec; //持てるコストの最大値,現在のコスト所持
+    private int[] partyChara; //パーティーのキャラ編成
+
+    //UI関連のオブジェクト
+    public Text costTxt;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //毎フレーム最初にgamespeedを考慮したdeltatimeを算出
+        gameDeltaTime = Time.deltaTime * playSpeed;
+
+        //コスト上昇処理
+        costValue += costUpVec * gameDeltaTime;
+        costTxt.text = costValue.ToString("000.00");
+    }
+
+    void levelUp()
+    {
+    }
+}
